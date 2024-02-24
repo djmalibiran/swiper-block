f90a4dcb4857cb7eebec36103a82087dd4e5847e
Daryl Malibiran <33365949+djmalibiran@users.noreply.github.com>
2024-02-25 04:13:30 +0800

    Final refinement

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'ba05bcbf200e5beba4aa');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '20ec173ebe20adf65625');
